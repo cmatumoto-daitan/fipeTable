d77e94ce0e24773f4fc48b4f98cae37d7955d07b
Carolina Ayumi Matumoto <cmatumoto@daitan.com>
2018-05-09 09:40:15 -0300

    Updates to use eslint

--- src/app.js
+++ src/app.js
@@ -1,38 +1,39 @@
 import * as functions from './utils';
 
-export let typeSelect,
-  vehicleSelect,
-  modelSelect,
-  preco = document.createElement('h1');
+// can't export let.
+// first select
+const typeSelect = functions.createSelect('type', '200px', '500px');
+// second select
+const vehicleSelect = functions.createSelect('vehicle', '200px', '800px');
+// third select
+const modelSelect = functions.createSelect('model', '200px', '1100px');
+const preco = document.createElement('h1');
 
-export const url = ['http://fipeapi.appspot.com/api/1/carros'];
+const url = 'http://fipeapi.appspot.com/api/1/carros';
 
 const app = document.getElementById('app');
 
 function loadElements() {
-  //the title os fthe page
+  // the title of the page
   app.innerHTML = "<h1 align='center'>FIPE Table</h1>";
-  //first select    
-  typeSelect = functions.createSelect('type', '200px', '500px');
-  //second select
-  vehicleSelect = functions.createSelect('vehicle', '200px', '800px');
-  //third select    
-  modelSelect = functions.createSelect('model', '200px', '1100px');
 
-  //set the elements in document
+  // set the elements in document
   app.appendChild(typeSelect);
   app.appendChild(vehicleSelect);
   app.appendChild(modelSelect);
   app.appendChild(preco);
 
-  //fills the first select 
-  functions.getInfoFromFipeTable(url + '/marcas.json')
-  .then(
-    function(array){
-      functions.populateSelect(array, typeSelect);
-  })
-  .catch(function(e){alert(e);} 
-)};
-document.addEventListener('DOMContentLoaded', function () {
-  loadElements();
-});
+  // fills the first select
+  functions.getInfoFromFipeTable(`${url}/marcas.json`)
+    .then((array) => { functions.populateSelect(array, typeSelect); });
+}
+
+document.addEventListener('DOMContentLoaded', () => { loadElements(); });
+
+export default {
+  typeSelect,
+  vehicleSelect,
+  modelSelect,
+  preco,
+  url,
+};
--- src/utils.js
+++ src/utils.js
@@ -1,86 +1,83 @@
-import * as components from "./app";
+import components from './app';
 
-let typeId, vehicleId, modelId;
+let typeId;
+let vehicleId;
+let modelId;
 
-//receives a select and by the 'id' determines what to do
-export function updateSelects(select) {
-  if (select.target.selectedIndex !== -1) {
-    components.preco.innerHTML = "";
-    const id = select.target.parentElement.value;
-    if (select.target.parentElement.id === 'type') {
-      typeId = id;
-      clearSelect(components.vehicleSelect);
-      getInfoFromFipeTable(components.url + '/veiculos/' + typeId + '.json')
-      .then(function (array) {
-        populateSelect(array, components.vehicleSelect);
-      });
-      clearSelect(components.modelSelect);
-    } else if (select.target.parentElement.id === 'vehicle') {
-      vehicleId = id;
-      clearSelect(components.modelSelect);
-      getInfoFromFipeTable(components.url + '/veiculo/' + typeId + '/' + vehicleId + '.json')
-      .then(function (array) {
-        populateSelect(array, components.modelSelect);
-      });
-    } else {
-      modelId = id;
-      getInfoFromFipeTable(components.url + '/veiculo/' + typeId + '/' + vehicleId + '/' + modelId + '.json')
-      .then(function (array) {
-        components.preco.innerHTML = "<br><h1 align='center'>" + (array['preco']) + "</h1>";
-      });
-    }
-  }
-}
-
-export function createSelect(id, width = '100px', left, size = 4) {
-  const select = document.createElement("select");
-  select.id = id;
-  select.size = size;
-  select.style.width = width;
-  select.style.position = 'absolute';
-  select.style.left = left;
-  select.addEventListener("click", updateSelects);
-  return select;
-}
-
-//clear the select
+// clear the select
 export function clearSelect(select) {
   while (select.options.length) {
     select.remove(0);
   }
 }
 
-//the data returned by the get is inserted in the select determined
-export function populateSelect(array, actualSelect) {
-  let select = addAnOptionInSelect(actualSelect);
-  array.forEach(element => {
-    select(element['name'], element['id']);
-  });
-}
-
-//when the select is detemined, returns a function that fills the select with options
-export function addAnOptionInSelect(elemSelect) {
-  return function (name, value) {
-    const elemOption = document.createElement("option");
-    elemOption.text = name;
-    elemOption.value = value;
-    elemSelect.add(elemOption, null);
-  }
-}
-
-//catch the information by the url and returns a promisse with the result
+// catch the information by the url and returns a promisse with the result
 export function getInfoFromFipeTable(urlSend) {
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     const url = urlSend;
     xhr.responseType = 'json';
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = () => {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         resolve(xhr.response);
       }
-    }
-    xhr.open('GET', url);
+    };
+    xhr.onerror = () => { reject(xhr.statusText); };
+    xhr.open('GET', url, true);
     xhr.send();
   });
-  
+}
+
+// when the select is detemined, returns a function that fills the select with options
+export function addAnOptionInSelect(elemSelect) {
+  return (name, value) => {
+    const elemOption = document.createElement('option');
+    elemOption.text = name;
+    elemOption.value = value;
+    elemSelect.add(elemOption, null);
+  };
+}
+
+// the data returned by the get is inserted in the select determined
+export function populateSelect(array, actualSelect) {
+  const select = addAnOptionInSelect(actualSelect);
+  array.forEach(element => (select(element.name, element.id)));
+}
+
+// receives a select and by the 'id' determines what to do
+export function updateSelects(select) {
+  if (select.target.selectedIndex !== -1) {
+    components.preco.innerHTML = '';
+    const id = select.target.parentElement.value;
+    if (select.target.parentElement.id === 'type') {
+      typeId = id;
+      clearSelect(components.vehicleSelect);
+      getInfoFromFipeTable(`${components.url}/veiculos/${typeId}.json`)
+        .then((array) => { populateSelect(array, components.vehicleSelect); })
+        .catch();
+      clearSelect(components.modelSelect);
+    } else if (select.target.parentElement.id === 'vehicle') {
+      vehicleId = id;
+      clearSelect(components.modelSelect);
+      getInfoFromFipeTable(`${components.url}/veiculo/${typeId}/${vehicleId}.json`)
+        .then((array) => { populateSelect(array, components.modelSelect); })
+        .catch();
+    } else {
+      modelId = id;
+      getInfoFromFipeTable(`${components.url}/veiculo/${typeId}/${vehicleId}/${modelId}.json`)
+        .then((array) => { components.preco.innerHTML = (`<br><h1 align='center'>${array.preco}</h1>`); })
+        .catch();
+    }
+  }
+}
+
+export function createSelect(id, width = '100px', left, size = 4) {
+  const select = document.createElement('select');
+  select.id = id;
+  select.size = size;
+  select.style.width = width;
+  select.style.position = 'absolute';
+  select.style.left = left;
+  select.addEventListener('click', updateSelects);
+  return select;
 }
