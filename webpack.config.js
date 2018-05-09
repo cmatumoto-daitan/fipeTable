037415d4e142248f05e51e521fc5eb74f9714d7a
Carolina Ayumi Matumoto <cmatumoto@daitan.com>
2018-05-09 14:21:11 -0300

    Starting to use async/await

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
+
 module.exports = {
-    //define entry point
-    entry: './src/app.js',
-    //define output point
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader'
-        }]
-    },
-    mode: 'development'
-}
+  // define entry point
+  entry: './src/app.js',
+  // define output point
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+    }],
+  },
+  mode: 'development',
+};
